49c59085a30a20ee0443020d956323f74e99cc17
Yaswanth.Chinnam <Yaswanth.Chinnam@BRILAP2841.BRILLIO.COM>
2022-09-08 14:14:32 +0530

    Day2Spring Project

---
+++ Day2SpringProject/src/com/brillio/TextEditor.java
@@ -0,0 +1,21 @@
+package com.brillio;
+
+public class TextEditor {
+	public SpellChecker spellChecker;
+
+	public TextEditor(SpellChecker spellChecker) {
+		super();
+		this.spellChecker = spellChecker;
+	}
+
+	public SpellChecker getSpellChecker() {
+		return spellChecker;
+	}
+
+	public void setSpellChecker(SpellChecker spellChecker) {
+		this.spellChecker = spellChecker;
+	}
+	
+	
+
+}
