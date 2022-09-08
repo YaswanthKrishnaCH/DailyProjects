24be5b0fd5dd8116c643748031da5092a441d4dc
Yaswanth.Chinnam <Yaswanth.Chinnam@BRILAP2841.BRILLIO.COM>
2022-09-08 14:12:47 +0530

    First Spring Example

---
+++ FirstSpringExample/src/Day9/Student.java
@@ -0,0 +1,21 @@
+package Day9;
+
+public class Student {
+	String Student_name;
+
+	
+	public String getStudent_name() {
+		return Student_name;
+	}
+
+	public void setStudent_name(String student_name) {
+		Student_name = student_name;
+	}
+
+	public void display() {
+		System.out.println("Within student class.....");
+	}
+
+	
+
+}
