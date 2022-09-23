7f1e2a67c63a86d54701375579b0510339a29252
Yaswanth.Chinnam <Yaswanth.Chinnam@BRILAP2841.BRILLIO.COM>
2022-09-23 09:22:10 +0530

    Done

---
+++ SpringEureka/src/test/java/com/Day12/SpringEureka/SpringEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Day12.SpringEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
